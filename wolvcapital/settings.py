62156b81a8681f1cb08178c9fc2458cc362f3f7e
sollid-web <ozoaniphilip49@gmail.com>
2026-03-01 03:18:07 +0100

    Final fix: switched to CompressedStaticFilesStorage

--- wolvcapital/settings.py
+++ wolvcapital/settings.py
@@ -314,19 +314,14 @@ TESTING = any(
     for arg in ["::"]
 ) or any(c in " ".join(sys.argv) for c in ["test", "pytest"])
 
-WHITENOISE_MANIFEST_STRICT = False
 
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
     "staticfiles": {
-        "BACKEND": (
-            "django.contrib.staticfiles.storage.StaticFilesStorage"
-            if (DEBUG or TESTING)
-            else "whitenoise.storage.CompressedManifestStaticFilesStorage"
-        ),
-    },
+    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+},
+
 }
 
 MEDIA_URL = "/media/"

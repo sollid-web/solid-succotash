d9923af04f12468381b785cc035924b842d1a7c6
sollid-web <ozoaniphilip49@gmail.com>
2026-03-06 05:24:36 +0100

    added email template

---
+++ core/management/commands/send_virtual_card_email.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.template.loader import render_to_string
+
+class Command(BaseCommand):
+    help = "Send WolvCapital Virtual Card email to investors"
+
+    def handle(self, *args, **kwargs):
+        # List of recipients
+        recipients = [
+            "teddy@example.com",
+            "jamescodegh@gmail.com",
+            # Add more emails as needed
+        ]
+
+        # Path to your HTML template in Django templates folder
+        html_template = "virtual_card_announcement.html"
+
+        for email in recipients:
+            # Render template
+            html_content = render_to_string(html_template, {})
+
+            # Email subject
+            subject = "Introducing Your WolvCapital Virtual Card"
+
+            # Build email message
+            msg = EmailMultiAlternatives(
+                subject=subject,
+                body="This is an HTML email. Please enable HTML view.",
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[email]
+            )
+            msg.attach_alternative(html_content, "text/html")
+
+            # Send email
+            msg.send()
+            self.stdout.write(self.style.SUCCESS(f"Email sent to {email}"))

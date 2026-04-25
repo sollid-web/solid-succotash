c6933888aff298d9b8abd92f332f167ab887d3d7
code <ozoaniphilip49@gmail.com>
2026-04-25 01:56:06 +0100

    added

--- cards/urls.py
+++ cards/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CardDetailView, CardFreezeView, CardTransactionsView, VerifyPasswordView, SetPinView
+from .views import CardDetailView, CardFreezeView, CardTransactionsView, VerifyPasswordView, SetPinView, CheckPinView
 
 urlpatterns = [
     path("", CardDetailView.as_view(), name="card-detail"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("transactions/", CardTransactionsView.as_view(), name="card-transactions"),
     path("verify-password/", VerifyPasswordView.as_view(), name="card-verify-password"),
     path("set-pin/", SetPinView.as_view(), name="card-set-pin"),
+    path("check-pin/", CheckPinView.as_view(), name="card-check-pin"),
 ]

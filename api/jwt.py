feeb8e166b44226ae5730b783152face83288554
code <ozoaniphilip49@gmail.com>
2026-05-15 04:46:19 +0100

    fix: pass attrs not dict to super().validate() in JWT serializer

--- api/jwt.py
+++ api/jwt.py
@@ -1,35 +1,31 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 
 class EmailOrUsernameTokenObtainPairSerializer(TokenObtainPairSerializer):
     """Allow JWT login with either email or username."""
 
-    email = serializers.EmailField(required=False)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Allow email-only login; username will be resolved from email.
         self.fields[self.username_field].required = False
+        self.fields['email'] = self.fields.get('email', __import__('rest_framework').serializers.EmailField(required=False))
 
     def validate(self, attrs):
-        data = dict(attrs)
-        email = data.get("email")
-        username = data.get(self.username_field)
+        email = attrs.get("email")
+        username = attrs.get(self.username_field)
 
         if email and not username:
+            User = get_user_model()
             user = (
-                get_user_model()
-                .objects.filter(email__iexact=email)
+                User.objects
+                .filter(email__iexact=email)
                 .only(self.username_field)
                 .first()
             )
             if user:
-                data[self.username_field] = getattr(user, self.username_field)
+                attrs[self.username_field] = getattr(user, self.username_field)
 
-        return super().validate(data)
+        return super().validate(attrs)
 
 
 class EmailOrUsernameTokenObtainPairView(TokenObtainPairView):

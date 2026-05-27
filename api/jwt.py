3d748d38967fd853d0eb306b47a48c2edcc06628
code <ozoaniphilip49@gmail.com>
2026-05-27 12:53:33 +0100

    fix: JWT login by email for all users

--- api/jwt.py
+++ api/jwt.py
@@ -1,29 +1,43 @@
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
 class EmailOrUsernameTokenObtainPairSerializer(TokenObtainPairSerializer):
-    """Allow JWT login with either email or username."""
+    """Allow JWT login with email and password."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields[self.username_field].required = False
-        self.fields['email'] = self.fields.get('email', __import__('rest_framework').serializers.EmailField(required=False))
+        # Remove the username field requirement, add email
+        self.fields.pop(self.username_field, None)
+        self.fields['email'] = serializers.EmailField(required=True)
+        self.fields['password'] = serializers.CharField(
+            required=True, write_only=True, trim_whitespace=False
+        )
 
     def validate(self, attrs):
-        email = attrs.get("email")
-        username = attrs.get(self.username_field)
+        User = get_user_model()
+        email = attrs.get('email')
+        password = attrs.get('password')
 
-        if email and not username:
-            User = get_user_model()
-            user = (
-                User.objects
-                .filter(email__iexact=email)
-                .only(self.username_field)
-                .first()
-            )
-            if user:
-                attrs[self.username_field] = getattr(user, self.username_field)
+        if not email or not password:
+            raise serializers.ValidationError('Email and password are required.')
+
+        # Find user by email
+        user = User.objects.filter(email__iexact=email).first()
+        if not user:
+            raise serializers.ValidationError('No account found with this email.')
+
+        if not user.check_password(password):
+            raise serializers.ValidationError('Incorrect password.')
+
+        if not user.is_active:
+            raise serializers.ValidationError('This account is inactive.')
+
+        # Inject username so parent token generation works
+        attrs[self.username_field] = getattr(user, self.username_field)
+        attrs['password'] = password
 
         return super().validate(attrs)
 
